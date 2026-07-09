b7316117c5e8e74a16d6cbf9c1c9925df61f6a6a
Vimal <vimalavk025@gmail.com>
2026-07-09 21:33:17 +0530

    Add files via upload

---
+++ software/C/cpp_level1/cp2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num,a,b;
+
+    cin>>num;
+
+    if(num>18)
+    {
+        cout<<"above age"<<num;
+
+    }
+    else
+    {
+        cout<<"below age"<<num;
+    }
+    return 0;
+}
